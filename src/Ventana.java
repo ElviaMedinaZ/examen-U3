841fb3d0b4b0028a4b2395ec68f5b97a0e155c46
ElviaMedinaZ <emedina_21@alu.uabcs.mx>
2024-04-27 12:17:15 -0700

    Creacion de diseño de registro y JMenu

--- src/Ventana.java
+++ src/Ventana.java
@@ -3,22 +3,35 @@ import java.awt.Image;
 import javax.swing.JFrame;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButton;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.ListSelectionModel;
 
 public class Ventana {
 
 	private JFrame frmBajaGym;
+	private JTextField txtApellidoPaterno;
+	private JTextField txtTelefono;
 	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtNombre;
 
 	/**
 	 * Launch the application.
@@ -42,6 +55,8 @@ public class Ventana {
 	 */
 	public Ventana() {
 		initialize();
+	   
+	
 	}
 
 	/**
@@ -49,14 +64,21 @@ public class Ventana {
 	 */
 	private void initialize() {
 		frmBajaGym = new JFrame();
-		
 		frmBajaGym.setTitle("Baja Gym");
-		frmBajaGym.setBounds(100, 100, 700, 700);
+		frmBajaGym.setBounds(100, 100, 899, 698);
 		frmBajaGym.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		inicio(frmBajaGym);
+	    login(frmBajaGym);
+	    registro1(frmBajaGym);
+	  
+		
+		
 		
 		// -------------------------- NOTA: INVESTIGAR BIEN LO DE LOS ICONOS -------------------------
 		
-		//frmBajaGym.setIconImage(Toolkit.getDefaultToolkit().getImage("\src\\icono.png"));
+		frmBajaGym.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/icono.png")));
+
 		
 		// Obtiene el tamaño de la pantalla
         int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
@@ -65,58 +87,265 @@ public class Ventana {
         //Guarda el tamaño de la ventana
         int ventanaWidth = 899;
         int ventanaHeight = 698;
-        frmBajaGym.setSize(879, 698);
+        frmBajaGym.setSize(759, 690);
 
         // Calculo de la pos
         int ventanaX=(screenWidth - ventanaWidth)/2;
         int ventanaY=(screenHeight - ventanaHeight)/2;
         frmBajaGym.setLocation(ventanaX, ventanaY);
-		frmBajaGym.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		frmBajaGym.getContentPane().setLayout(null);
+		
+		
+		
         
+		//////////////////////////////////////////////////////////////// MENU //////////////////////////////////////////////////////////////////
+		
+        JMenuBar menuBar = new JMenuBar();
+        frmBajaGym.setJMenuBar(menuBar);
+        
+        //////////////////////////////////////////////// USUARIOS ///////////////////////////////////////////
+        
+        JMenu mnUsuarios = new JMenu("Usuarios");
+        menuBar.add(mnUsuarios);
+        
+        JMenuItem mntmIniciarSesion = new JMenuItem("Iniciar sesion");
+        mntmIniciarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				frmBajaGym.getContentPane().removeAll();
+				login(frmBajaGym);
+				frmBajaGym.validate();
+				frmBajaGym.repaint();
+			}
+		});
+        mnUsuarios.add(mntmIniciarSesion);
+        
+       
+        
+        JMenuItem mnRegistro = new JMenuItem("Registro");
+        mnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				frmBajaGym.getContentPane().removeAll();
+				registro1(frmBajaGym);
+				frmBajaGym.validate();
+				frmBajaGym.repaint();
+			}
+		});
+        mnUsuarios.add(mnRegistro);
+        
+        
+        //////////////////////////////////////////////// CLIENTES  ///////////////////////////////////////////
+        
+        JMenu mnClientes = new JMenu("Clientes");
+        menuBar.add(mnClientes);
+        
+        JMenuItem mntmCrearCliente = new JMenuItem("Crear");
+        mnClientes.add(mntmCrearCliente);
+        
+        JMenuItem mntmEditarClientes = new JMenuItem("Editar");
+        mnClientes.add(mntmEditarClientes);
+        
+        JMenuItem mntmEliminarClientes = new JMenuItem("Eliminar");
+        mnClientes.add(mntmEliminarClientes);
+        
+        JMenuItem mntmConsultarClientes = new JMenuItem("Consultar");
+        mnClientes.add(mntmConsultarClientes);
+        
+        //////////////////////////////////////////////// TARIFAS  ///////////////////////////////////////////
+        
+        JMenu Tarifas = new JMenu("Tarifas");
+        menuBar.add(Tarifas);
+        
+        JMenu mnConsultarTarifa = new JMenu("Consultar");
+        Tarifas.add(mnConsultarTarifa);
+        
+        JMenuItem mntmCrearTarifa = new JMenuItem("Crear");
+        Tarifas.add(mntmCrearTarifa);
+        
+        JMenuItem mntmEditarTarifa = new JMenuItem("Editar");
+        Tarifas.add(mntmEditarTarifa);
+        
+        JMenuItem mntmEliminarTarifa = new JMenuItem("Eliminar");
+        Tarifas.add(mntmEliminarTarifa);
+        
+        //////////////////////////////////////////////// TIENDA  ///////////////////////////////////////////
+        
+        JMenu mnTienda = new JMenu("Tienda");
+        menuBar.add(mnTienda);
+        
+        JMenu mnMujeres = new JMenu("Mujeres");
+        mnTienda.add(mnMujeres);
+        
+        JMenuItem mntmHombres = new JMenuItem("Hombres");
+        mnTienda.add(mntmHombres);
+        
+        JMenuItem mntmSuplementos = new JMenuItem("Suplementos");
+        mnTienda.add(mntmSuplementos);
+        
+        JMenuItem mntmEquipo = new JMenuItem("Equipo");
+        mnTienda.add(mntmEquipo);
+        
+        //////////////////////////////////////////////// CAJA  ///////////////////////////////////////////
+        
+        JMenu mnCaja = new JMenu("Caja");
+        menuBar.add(mnCaja);
+        
+        JMenuItem mntmFactura = new JMenuItem("Factura");
+        mnCaja.add(mntmFactura);
+	}
+	
+	
+public void login (JFrame frame) {
+		
 		JPanel panelLogin = new JPanel();
-        frmBajaGym.getContentPane().add(panelLogin);
+		panelLogin.setBounds(-42, 0, 863, 637);
+        frame.getContentPane().add(panelLogin);
         panelLogin.setBackground(Color.decode("#071931"));
         panelLogin.setLayout(null);
         
-        JLabel lblNewLabel = new JLabel("Inicio de sesíon");
-        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel.setVerticalAlignment(SwingConstants.CENTER);
-        lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 40));
-        lblNewLabel.setForeground(new Color(255, 255, 255));
-        lblNewLabel.setBounds(212, 40, 428, 45);
-        panelLogin.add(lblNewLabel);
+        JLabel lbl_InicioSeSion = new JLabel("Inicio de sesíon");
+        lbl_InicioSeSion.setHorizontalAlignment(SwingConstants.CENTER);
+        lbl_InicioSeSion.setVerticalAlignment(SwingConstants.CENTER);
+        lbl_InicioSeSion.setFont(new Font("Dialog", Font.BOLD, 40));
+        lbl_InicioSeSion.setForeground(new Color(255, 255, 255));
+        lbl_InicioSeSion.setBounds(223, 189, 428, 45);
+        panelLogin.add(lbl_InicioSeSion);
         
-        JLabel lblNewLabel_1 = new JLabel("Usuario");
-        lblNewLabel_1.setForeground(new Color(153, 153, 153));
-        lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-        lblNewLabel_1.setBounds(82, 188, 109, 24);
-        panelLogin.add(lblNewLabel_1);
+        JTextField txtNombreDeUsuario = new JTextField();
+        txtNombreDeUsuario.setForeground(new Color(128, 128, 128));
+        txtNombreDeUsuario.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        txtNombreDeUsuario.setText("Nombre de usuario");
+        txtNombreDeUsuario.setBounds(215, 372, 462, 37);
+        panelLogin.add(txtNombreDeUsuario);
+        txtNombreDeUsuario.setColumns(10);
         
-        textField = new JTextField();
-        textField.setBounds(72, 180, 706, 45);
-        panelLogin.add(textField);
-        textField.setColumns(10);
+        JTextField txtContraseña = new JTextField();
+        txtContraseña.setForeground(new Color(128, 128, 128));
+        txtContraseña.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        txtContraseña.setText("Contraseña");
+        txtContraseña.setColumns(10);
+        txtContraseña.setBounds(215, 286, 462, 37);
+        panelLogin.add(txtContraseña);
         
-        textField_1 = new JTextField();
-        textField_1.setColumns(10);
-        textField_1.setBounds(72, 279, 706, 45);
-        panelLogin.add(textField_1);
+        JLabel lblUser = new JLabel("");
+        lblUser.setIcon(new ImageIcon(getClass().getResource("userxd.png")));
+        lblUser.setBounds(155,273,50,50);
+		panelLogin.add(lblUser);
+	
         
-        JLabel lblNewLabel_1_1 = new JLabel("Contraseña");
-        lblNewLabel_1_1.setForeground(new Color(153, 153, 153));
-        lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-        lblNewLabel_1_1.setBounds(72, 283, 161, 34);
-        panelLogin.add(lblNewLabel_1_1);
+        JLabel logoContraseña = new JLabel();
+	    logoContraseña.setIcon(new ImageIcon(getClass().getResource("candadoBlanco.png")));
+	    logoContraseña.setBounds(155,359,40,50);
+	    panelLogin.add(logoContraseña);
+	    
+	    JLabel lblLogo = new JLabel();
+        lblLogo.setIcon(new ImageIcon(getClass().getResource("logoLogin.png")));
+        lblLogo.setBounds(368,0,229,220);
+	    panelLogin.add(lblLogo);
         
-        JButton btnNewButton = new JButton("Ingresar");
-        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        btnNewButton.setBounds(259, 471, 246, 68);
-        panelLogin.add(btnNewButton);
+        JLabel lblContraOlvidada = new JLabel("¿Olvidaste tu contraseña?");
+        lblContraOlvidada.setFont(new Font("Tahoma", Font.ITALIC, 15));
+        lblContraOlvidada.setForeground(new Color(255, 255, 255));
+        lblContraOlvidada.setBounds(352, 584, 180, 14);
         
-        JLabel lblNewLabel_1_2 = new JLabel("Contraseña");
-        lblNewLabel_1_2.setForeground(new Color(153, 153, 153));
-        lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 24));
-        lblNewLabel_1_2.setBounds(82, 283, 208, 35);
-        panelLogin.add(lblNewLabel_1_2);
+        panelLogin.add(lblContraOlvidada);
+        
+        JLabel lblNoCuenta = new JLabel("¿No tienes cuenta? Registrate aquí");
+        lblNoCuenta.setFont(new Font("Tahoma", Font.ITALIC, 15));
+        lblNoCuenta.setForeground(Color.WHITE);
+        lblNoCuenta.setBounds(318, 561, 238, 25);
+        panelLogin.add(lblNoCuenta);
+		
 	}
+
+
+
+
+public void inicio (JFrame frame) {
+
+	JPanel panelInicio = new JPanel();
+	panelInicio.setBackground(Color.decode("#071931"));
+	panelInicio.setBounds(0, 0, 814, 622);
+	frmBajaGym.getContentPane().add(panelInicio);
+	 panelInicio.setLayout(null);
+	
+	 JLabel lblFondo = new JLabel();
+	 lblFondo.setIcon(new ImageIcon(getClass().getResource("icono.png")));
+     lblFondo.setBounds(72,52,663,508);
+     panelInicio.add(lblFondo);
+}
+
+
+public void registro1(JFrame frame) {
+	JPanel panelRegistro = new JPanel();
+	panelRegistro.setBounds(0, -25, 857, 759);
+    frame.getContentPane().add(panelRegistro);
+    panelRegistro.setBackground(Color.decode("#071931"));
+    panelRegistro.setLayout(null);
+    
+    JLabel lbl_InicioSeSion = new JLabel("Registro");
+    lbl_InicioSeSion.setHorizontalAlignment(SwingConstants.CENTER);
+    lbl_InicioSeSion.setVerticalAlignment(SwingConstants.CENTER);
+    lbl_InicioSeSion.setFont(new Font("Dialog", Font.BOLD, 40));
+    lbl_InicioSeSion.setForeground(new Color(255, 255, 255));
+    lbl_InicioSeSion.setBounds(155, 168, 428, 45);
+    panelRegistro.add(lbl_InicioSeSion);
+    
+    JLabel lblUser = new JLabel("");
+    lblUser.setBounds(155,273,50,50);
+    panelRegistro.add(lblUser);
+
+    
+    JLabel logoContraseña = new JLabel();
+    logoContraseña.setBounds(155,359,40,50);
+    panelRegistro.add(logoContraseña);
+    
+    JLabel lblLogo = new JLabel();
+    lblLogo.setIcon(new ImageIcon(getClass().getResource("logoLogin.png")));
+    lblLogo.setBounds(299,-7,229,220);
+    panelRegistro.add(lblLogo);
+    
+    txtApellidoPaterno = new JTextField();
+    txtApellidoPaterno.setText("Apellido Paterno");
+    txtApellidoPaterno.setForeground(Color.GRAY);
+    txtApellidoPaterno.setFont(new Font("Tahoma", Font.PLAIN, 20));
+    txtApellidoPaterno.setColumns(10);
+    txtApellidoPaterno.setBounds(165, 390, 428, 45);
+    panelRegistro.add(txtApellidoPaterno);
+    
+    txtTelefono = new JTextField();
+    txtTelefono.setText("Telefono");
+    txtTelefono.setForeground(Color.GRAY);
+    txtTelefono.setFont(new Font("Tahoma", Font.PLAIN, 20));
+    txtTelefono.setColumns(10);
+    txtTelefono.setBounds(165, 465, 428, 45);
+    panelRegistro.add(txtTelefono);
+    
+    textField = new JTextField();
+    textField.setText("Apellido Materno");
+    textField.setForeground(Color.GRAY);
+    textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+    textField.setColumns(10);
+    textField.setBounds(165, 313, 428, 45);
+    panelRegistro.add(textField);
+    
+    txtNombre = new JTextField();
+    txtNombre.setText("Nombre");
+    txtNombre.setForeground(Color.GRAY);
+    txtNombre.setFont(new Font("Tahoma", Font.PLAIN, 20));
+    txtNombre.setColumns(10);
+    txtNombre.setBounds(165, 242, 428, 45);
+    panelRegistro.add(txtNombre);
+    
+    JButton btnAtras = new JButton("");
+    btnAtras.setBounds(165, 555, 174, 57);
+    panelRegistro.add(btnAtras);
+    
+    JButton btnSiguient = new JButton("");
+    btnSiguient.setBounds(388, 555, 174, 57);
+    panelRegistro.add(btnSiguient);
+
+	
+}
 }
